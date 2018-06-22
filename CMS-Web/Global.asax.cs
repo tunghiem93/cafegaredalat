c2b9ec0c3983bf6c0d2b3eecfd5af9a2eb056d9e
Quang Tran <quangtrantpq@gmail.com>
2018-06-22 15:41:04 +0700

    [M] NSLog: update config for log.

--- CMS-Web/Global.asax.cs
+++ CMS-Web/Global.asax.cs
@@ -17,6 +17,7 @@ namespace CMS_Web
     {
         protected void Application_Start()
         {
+            log4net.Config.XmlConfigurator.Configure();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
